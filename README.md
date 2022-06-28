c151d8cd4142011fa4a4d54e704992a7ec86491c
VladyslavBabenko <babenkovladyslav1@gmail.com>
2022-06-28 20:18:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ColorDetector
+The core of this little project is ability to analyze input image and output in % each color encountered
---
+++ src/main/java/com/github/vladyslavbabenko/colordetector/ColorDetectorApplication.java
@@ -0,0 +1,11 @@
+package com.github.vladyslavbabenko.colordetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorDetectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ColorDetectorApplication.class, args);
+    }
+}
